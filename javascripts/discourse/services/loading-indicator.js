d48c595cd9d467a52c57a12f5d3d5244686bcd6c
David Taylor <david@taylorhq.com>
2021-02-04 20:39:11 +0000

    UX: Fade out page while loading

--- javascripts/discourse/services/loading-indicator.js
+++ javascripts/discourse/services/loading-indicator.js
@@ -1,14 +1,21 @@
 import Service from "@ember/service";
 import Evented from "@ember/object/evented";
+import { schedule } from "@ember/runloop";
 
 export default Service.extend(Evented, {
   start() {
     this.set("loading", true);
     this.trigger("stateChanged", true);
+    schedule("afterRender", () => {
+      document.body.classList.add("loading");
+    });
   },
 
   end() {
     this.set("loading", false);
     this.trigger("stateChanged", false);
+    schedule("afterRender", () => {
+      document.body.classList.remove("loading");
+    });
   },
 });
