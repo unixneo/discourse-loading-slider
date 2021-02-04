ad14e1a01b6bd8e46de5b4b4983a8ee1417e7cef
David Taylor <david@taylorhq.com>
2021-02-04 19:31:56 +0000

    UX: Keep bar moving for up to 5s during loading
    Refactors to use CSS transitions rather than animations. This allows
    the final 'loaded' state to start from wherever the bar is currently at

--- javascripts/discourse/components/loading-indicator.js
+++ javascripts/discourse/components/loading-indicator.js
@@ -1,13 +1,42 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
-import discourseComputed from "discourse-common/utils/decorators";
-import { readOnly } from "@ember/object/computed";
+import { equal } from "@ember/object/computed";
+import { next } from "@ember/runloop";
 
 export default Component.extend({
   loadingIndicator: service(),
 
   classNames: "loading-indicator-container",
-  classNameBindings: "loading",
+  classNameBindings: ["ready", "loading", "done"],
 
-  loading: readOnly("loadingIndicator.loading"),
+  state: "ready",
+
+  ready: equal("state", "ready"),
+  loading: equal("state", "loading"),
+  done: equal("state", "done"),
+
+  stateChanged(loading) {
+    if (loading && this.ready) {
+      this.set("state", "loading");
+    } else if (loading) {
+      this.set("state", "ready");
+      next(() => this.set("state", "loading"));
+    } else {
+      this.set("state", "done");
+    }
+  },
+
+  didInsertElement() {
+    this.loadingIndicator.on("stateChanged", this, "stateChanged");
+
+    this.element.addEventListener("transitionend", (event) => {
+      if (event.target == this.element && event.propertyName == "opacity") {
+        this.set("state", "ready");
+      }
+    });
+  },
+
+  willDestroyElement() {
+    this.loadingIndicator.off("stateChange", this, "stateChange");
+  },
 });
--- javascripts/discourse/services/loading-indicator.js
+++ javascripts/discourse/services/loading-indicator.js
@@ -1,12 +1,14 @@
 import Service from "@ember/service";
-import { get } from "@ember/object";
+import Evented from "@ember/object/evented";
 
-export default Service.extend({
+export default Service.extend(Evented, {
   start() {
     this.set("loading", true);
+    this.trigger("stateChanged", true);
   },
 
   end() {
     this.set("loading", false);
+    this.trigger("stateChanged", false);
   },
 });
