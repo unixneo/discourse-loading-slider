0f06fccd83dfe525c4d4a0e816e8a29a9305b487
David Taylor <david@taylorhq.com>
2021-02-05 11:36:27 +0000

    UX: Use average loading time to set progress bar speed

--- javascripts/discourse/components/loading-indicator.js
+++ javascripts/discourse/components/loading-indicator.js
@@ -2,17 +2,20 @@ import Component from "@ember/component";
 import { inject as service } from "@ember/service";
 import { equal } from "@ember/object/computed";
 import { next } from "@ember/runloop";
+import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
   loadingIndicator: service(),
 
   classNames: "loading-indicator-container",
-  classNameBindings: ["ready", "loading", "done"],
+  classNameBindings: ["ready", "loading", "stillLoading:still-loading", "done"],
+  attributeBindings: "style",
 
   state: "ready",
 
   ready: equal("state", "ready"),
   loading: equal("state", "loading"),
+  stillLoading: equal("state", "still-loading"),
   done: equal("state", "done"),
 
   stateChanged(loading) {
@@ -26,12 +29,25 @@ export default Component.extend({
     }
   },
 
+  @discourseComputed("loadingIndicator.averageTime")
+  style(averageTime) {
+    return `--loading-duration: ${averageTime.toFixed(2)}s;`.htmlSafe();
+  },
+
   didInsertElement() {
     this.loadingIndicator.on("stateChanged", this, "stateChanged");
+
+    const bar = this.element.querySelector(".loading-indicator");
 
     this.element.addEventListener("transitionend", (event) => {
       if (event.target == this.element && event.propertyName == "opacity") {
         this.set("state", "ready");
+      } else if (
+        event.target === bar &&
+        event.propertyName == "width" &&
+        this.state == "loading"
+      ) {
+        this.set("state", "still-loading");
       }
     });
   },
--- javascripts/discourse/services/loading-indicator.js
+++ javascripts/discourse/services/loading-indicator.js
@@ -2,8 +2,20 @@ import Service from "@ember/service";
 import Evented from "@ember/object/evented";
 import { schedule } from "@ember/runloop";
 
+const STORE_LOADING_TIMES = 5;
+const DEFAULT_LOADING_TIME = 0.3;
+const MIN_LOADING_TIME = 0.1;
+
 export default Service.extend(Evented, {
+  init() {
+    this._super();
+    this.loadingTimes = [DEFAULT_LOADING_TIME];
+    this.set("averageTime", DEFAULT_LOADING_TIME);
+    this.i = 0;
+  },
+
   start() {
+    this.set("startedAt", Date.now());
     this.set("loading", true);
     this.trigger("stateChanged", true);
     schedule("afterRender", () => {
@@ -12,10 +24,25 @@ export default Service.extend(Evented, {
   },
 
   end() {
+    this.updateAverage((Date.now() - this.startedAt) / 1000);
     this.set("loading", false);
     this.trigger("stateChanged", false);
     schedule("afterRender", () => {
       document.body.classList.remove("loading");
     });
+  },
+
+  updateAverage(durationSeconds) {
+    if (durationSeconds < MIN_LOADING_TIME) {
+      durationSeconds = MIN_LOADING_TIME;
+    }
+
+    this.loadingTimes[this.i] = durationSeconds;
+
+    this.i = (this.i + 1) % STORE_LOADING_TIMES;
+    this.set(
+      "averageTime",
+      this.loadingTimes.reduce((p, c) => p + c, 0) / this.loadingTimes.length
+    );
   },
 });
